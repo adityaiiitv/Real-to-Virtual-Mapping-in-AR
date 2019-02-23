c1e12efccdeae79cbdb6b4b0c961a8411fcc5c91
Aditya Prakash <aditya.prakash101194@gmail.com>
2019-02-23 15:01:48 -0600

    Initial commit

---
+++ OpenCVUnity/Assets/Plugins/PositionAtFaceScreenSpace.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PositionAtFaceScreenSpace : MonoBehaviour
+{
+    private float _camDistance;
+
+    void Start()
+    {
+        _camDistance = Vector3.Distance(Camera.main.transform.position, transform.position);
+    }
+
+    void Update()
+    {
+        if (OpenCVFaceDetection.NormalizedFacePositions.Count == 0)
+            return;
+
+        transform.position = Camera.main.ViewportToWorldPoint(new Vector3(OpenCVFaceDetection.NormalizedFacePositions[0].x, OpenCVFaceDetection.NormalizedFacePositions[0].y, _camDistance));
+    }
+}
